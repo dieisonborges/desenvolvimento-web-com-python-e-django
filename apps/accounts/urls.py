b090de11c1302dd6c0ee4198830023e08282df00
Dieison Borges <dieisoncomix@gmail.com>
2020-04-03 22:30:45 -0300

    Profile

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     #user
     path('', views.list_users, name='list_users'),
     path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
+    path('alterar-senha/', views.user_change_password, name='user_change_password'),
+    path('perfil/', views.add_user_profile, name='add_user_profile'),
     path('adicionar/', views.add_user, name='add_user'),
     #path('editar/<int:id_user>/', views.edit_user, name='edit_user'),
     #path('apagar/<int:id_user>/', views.delete_user, name='delete_user'),
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
-from .forms import UserForm
-from django.contrib.auth import authenticate, login
+from .forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+
+#User Login
+def user_login(request):
+    template_name = 'accounts/user_login.html'
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login efetuado com sucesso!')
+            return redirect(request.GET.get('next', '/'))
+            #return HttpResponseRedirect(request.POST.get('next'))
+        else:
+            messages.error(request, 'Usuário ou senha inválidos')
+    return render(request, template_name, {})
 
 # Create your views here.
 @login_required(login_url='/usuarios/login/')
@@ -20,20 +38,6 @@ def add_user(request):
     form = UserForm()
     context['form'] = form
     return render(request, template_name, context)
-#User Login
-def user_login(request):
-    template_name = 'accounts/user_login.html'
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect(request.GET.get('next', '/'))
-            #return HttpResponseRedirect(request.POST.get('next'))
-        else:
-            messages.error(request, 'Usuário ou senha inválidos')
-    return render(request, template_name, {})
 
 @login_required(login_url='/usuarios/login/')
 def list_users(request):
@@ -44,3 +48,40 @@ def list_users(request):
     }
     return render(request, template_name, context)
 
+@login_required(login_url='/usuarios/login/')
+def user_logout(request):
+    logout(request)
+    return redirect('accounts:user_login')
+
+@login_required(login_url='/usuarios/login/')
+def user_change_password(request):
+    template_name = 'accounts/user_change_password.html'
+    context = {}
+    if request.method == 'POST':
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():            
+            f = form.save(commit=False)
+            f.set_password(f.password)
+            f.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, 'Senha modificada com sucesso!')
+        else:
+            messages.error(request, 'Não foi possível modificar sua senha')
+    form = PasswordChangeForm(user=request.user)
+    context['form'] = form
+    return render(request, template_name, context)
+
+@login_required(login_url='/usuarios/login/')
+def add_user_profile(request):
+    template_name = 'accounts/add_user_profile.html'
+    context = {}
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.user = request.user
+            f.save()
+            messages.success(request, 'Modificado com sucesso!')
+    form = UserProfileForm()
+    context['form'] = form
+    return render(request, template_name, context)
